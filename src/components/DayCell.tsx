46de05a0529e5a3b5e3f3d57c6b313882c01e302
roman-collinge <romancollinge@outlook.com>
2026-01-05 20:36:34 +0000

    feat: style DayCell component

--- src/components/DayCell.tsx
+++ src/components/DayCell.tsx
@@ -5,10 +5,9 @@ interface DayCellProps {
 function DayCell(props: DayCellProps) {
     const dayNumber = props.date.getDate()
     return (
-        <>
-            <p>This is my day cell component.</p>
-            <p>Day Number: {dayNumber}</p>
-        </>
+        <div className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 border border-gray-300 rounded flex items-center justify-center hover:bg-gray-100 cursor-pointer">
+            {dayNumber}
+        </div>
     )
 }
 
