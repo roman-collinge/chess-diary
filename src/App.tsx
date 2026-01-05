cdd4979b8589f93be27755dc7ab08a7a43ea2671
roman-collinge <romancollinge@outlook.com>
2026-01-05 17:31:23 +0000

    feat: add and verify tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import DayCell from "./components/DayCell"
 function App() {
   return (
     <>
-      <h1>Chess Diary</h1>
+      <h1 className="text-4xl font-bold">Chess Diary</h1>
       <DayCell date="05/01/2026" />
     </>
   )
