755f72238602092ca10b6934524055626175203c
roman-collinge <romancollinge@outlook.com>
2026-01-05 17:41:59 +0000

    feat: add and verify shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import DayCell from "./components/DayCell"
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <>
       <h1 className="text-4xl font-bold">Chess Diary</h1>
+      <Button>Test Button</Button>
       <DayCell date="05/01/2026" />
     </>
   )
