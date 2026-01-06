02a89ff359061bcc0d44240ab9308009d48172fc
roman-collinge <romancollinge@outlook.com>
2026-01-06 13:30:05 +0000

    feat: replace mock week with mock 31 day month

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,9 +1,4 @@
-export const mockDates: Date[] = [
-    new Date(2026, 0, 1),
-    new Date(2026, 0, 2),
-    new Date(2026, 0, 3),
-    new Date(2026, 0, 4),
-    new Date(2026, 0, 5),
-    new Date(2026, 0, 6),
-    new Date(2026, 0, 7),
-]
+export const mockDates: Date[] = Array.from(
+    { length: 31 },
+    (_, i) => new Date(2026, 0, i + 1)
+)
