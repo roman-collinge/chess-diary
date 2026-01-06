0d68a6b5cd2a12dc2aa376e7614d7c2ecc29afd6
roman-collinge <romancollinge@outlook.com>
2026-01-06 13:28:50 +0000

    Create moock week data

---
+++ src/mockData.ts
@@ -0,0 +1,9 @@
+export const mockDates: Date[] = [
+    new Date(2026, 0, 1),
+    new Date(2026, 0, 2),
+    new Date(2026, 0, 3),
+    new Date(2026, 0, 4),
+    new Date(2026, 0, 5),
+    new Date(2026, 0, 6),
+    new Date(2026, 0, 7),
+]
